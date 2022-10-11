ccc376953efbfcca39e143f9e15fe7064f62cd67
Tulsi <tulsiii1104@gmail.com>
2022-10-11 21:05:09 +0530

    Commiteddd

---
+++ app/src/main/java/com/example/madpractical_6_20012021041/YoutubeActivity.kt
@@ -0,0 +1,31 @@
+package com.example.madpractical_6_20012021041
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.widget.VideoView
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+class YoutubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState:Bundle?)
+
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+        val mywebview = findViewById<WebView>(R.id.wv)
+        val myactionbutton = findViewById<FloatingActionButton>(R.id.fb2)
+        myactionbutton.setOnClickListener(){
+
+            Intent(this,MainActivity::class.java).apply {
+                startActivity(this) }
+        }
+        val youtubeid = "d2fTjMt_l1M"
+        val webSettings:WebSettings = mywebview.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+
+        mywebview.loadUrl("https://www.youtube.com/embed/$youtubeid")
+    }
+}
